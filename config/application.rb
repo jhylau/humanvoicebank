f1d5c3efb45569dbdd33c06f65f6dbadb001d329
Jonathan Lau <jhylau1@gmail.com>
2014-02-11 22:02:04 -0500

    static assets

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Cleanpowerperks
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Deploy static assets
+    config.serve_static_assets = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
