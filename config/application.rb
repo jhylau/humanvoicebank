68eb0bd21591233d6bfb7e0a9a0c2587512ab3ea
Jonathan Lau <jhylau1@gmail.com>
2014-02-11 22:02:42 -0500

    static assets

--- config/application.rb
+++ config/application.rb
@@ -53,9 +53,6 @@ module Cleanpowerperks
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
     # Deploy static assets
     config.serve_static_assets = false
 
