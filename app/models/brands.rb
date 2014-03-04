83e8efd9607b42174e3c69f0b90618e2af98eaf5
Jonathan Lau <jhylau1@gmail.com>
2014-03-03 19:37:05 -0500

    fixed brands bug

--- app/models/brands.rb
+++ app/models/brands.rb
@@ -4,11 +4,11 @@ class Brand < ActiveRecord::Base
   
   has_many :perks
 
-  mount_uploader :image, ImageUploader
+  # mount_uploader :image, ImageUploader
 
   # validates_presence_of :category_id, :subtitle, :title
 
-  include PgSearch
+  # include PgSearch
   # pg_search_scope :perk_search, :against => [:title, :subtitle, :description, :website, :city, :state], :associated_against => {
   #   :category => [:title, :subtitle]
   # }
