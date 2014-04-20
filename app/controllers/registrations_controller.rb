5d1f01f02f072e799f27645ba4c4bc1448208228
Jonathan Lau <jhylau1@gmail.com>
2014-04-20 19:45:05 -0400

    commented out fog

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,20 +1,20 @@
-# controllers/users/registrations_controller.rb
-class Users::RegistrationsController < Devise::RegistrationsController
+# # controllers/users/registrations_controller.rb
+# class Users::RegistrationsController < Devise::RegistrationsController
  
-  before_filter :configure_permitted_parameters
+#   before_filter :configure_permitted_parameters
  
-  protected
+#   protected
  
-  # my custom fields are :name, :heard_how
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:name, :heard_how,
-        :email, :password, :password_confirmation)
-    end
-    devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:name,
-        :email, :password, :password_confirmation, :current_password)
-    end
-  end
+#   # my custom fields are :name, :heard_how
+#   def configure_permitted_parameters
+#     devise_parameter_sanitizer.for(:sign_up) do |u|
+#       u.permit(:name, :heard_how,
+#         :email, :password, :password_confirmation)
+#     end
+#     devise_parameter_sanitizer.for(:account_update) do |u|
+#       u.permit(:name,
+#         :email, :password, :password_confirmation, :current_password)
+#     end
+#   end
  
-end
+# end
